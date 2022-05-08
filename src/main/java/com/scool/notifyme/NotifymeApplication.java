3b5ba99d643c1ed27c4713f44aa29cc3c8cca4c5
SCool62 <79726681+SCool62@users.noreply.github.com>
2022-05-08 08:46:40 -0700

    Initial commit

---
+++ src/main/java/com/scool/notifyme/NotifymeApplication.java
@@ -0,0 +1,13 @@
+package com.scool.notifyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifymeApplication.class, args);
+	}
+
+}
