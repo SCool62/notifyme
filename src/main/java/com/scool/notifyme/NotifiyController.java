4dbea210e1ce29750a7bb9cbf48e7bf40f40b562
SCool62 <79726681+SCool62@users.noreply.github.com>
2022-05-15 09:23:28 -0700

    Check if the user is in the database before sending email

--- src/main/java/com/scool/notifyme/NotifiyController.java
+++ src/main/java/com/scool/notifyme/NotifiyController.java
@@ -2,8 +2,11 @@ package com.scool.notifyme;
 
 
 import com.scool.notifyme.email.EmailService;
+import com.scool.notifyme.user.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotifiyController {
 
     private final EmailService emailService;
+    private final UserService userService;
 
     @PostMapping(path = "/send/{to}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String sendEmail(@RequestBody String rawRequest,
-                            @PathVariable(name = "to") String to) {
+    public ResponseEntity<String> sendEmail(@RequestBody String rawRequest,
+                                    @PathVariable(name = "to") String to) {
         NotifyRequest request = NotifyConverter.convert(rawRequest);
+        if (userService.findByEmail(to).isEmpty() || userService.findByAlias(to).isEmpty()) {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        } else if (userService.findByAlias(to).isPresent()) {
+            to = userService.findByAlias(to).get().getEmail();
+        }
         emailService.send(to, request.get_subject(), request.getBody(), request.get_replyTo());
         if (request.get_redirect() != null) {
-            return "redirect:" + request.get_redirect();
+            return new ResponseEntity<>("redirect:" + request.get_redirect(), HttpStatus.OK);
         }
-        return "redirect:/success.html";
+        return new ResponseEntity<>("redirect:/success.html", HttpStatus.OK);
 
     }
 
